ae747adda140e992fc9b9e4c219997445ee50027
Aditya1092 <91484084+Aditya1092@users.noreply.github.com>
2023-07-03 21:54:08 +0530

    second

--- j.js
+++ j.js
@@ -1,27 +1,36 @@
 let display = document.getElementById('display');
+let numbersDisplay = document.getElementById('numbers-display');
+let numbersOperated = [];
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map( button => {
+buttons.map(button => {
     button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
+        switch (e.target.innerText) {
             case 'C':
                 display.innerText = '';
+                numbersOperated = [];
                 break;
             case '=':
-                try{
-                    display.innerText = eval(display.innerText);
+                try {
+                    let result = eval(display.innerText);
+                    display.innerText = result;
+                   
                 } catch {
-                    display.innerText = "Error"
+                    display.innerText = "Error";
                 }
                 break;
             case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
             default:
                 display.innerText += e.target.innerText;
+                numbersOperated.push(e.target.innerText);
         }
+
+        // Update numbers display
+        numbersDisplay.innerText = numbersOperated.join(' ');
     });
 });
